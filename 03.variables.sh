2e572f8486045940b6654809a11f29a3727e648b
Nagesh <nageshdevops8@gmail.com>
2025-08-27 23:26:12 +0530

    hello world started

---
+++ 03.variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+person1=Trump
+person2=Musk
+
+echo "$person1: Hi $person2, what are you doing"
+echo "$person2:am doing goood"
+echo "$person1: what's app $person2"
+echo "$person2: goin to Mars, will you come"
+echo "$person1: sorry, you carry on $person2. "
