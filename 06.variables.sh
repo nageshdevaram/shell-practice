7af4817cfa9637648d3581aa6233eca31b6510f8
Nagesh <nageshdevops8@gmail.com>
2025-08-31 22:12:58 +0530

    shell script updating

--- 06.variables.sh
+++ 06.variables.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+Number1=100
+Number2=300
+
 Timestamp=$(date)
 
 echo "Script executed at $Timestamp "
+
+sum=$(($Number1+$Number2))
+
+echo "the sum of Number 1 and Number 2 is : $sum"
