7440050b945658a4ec20ed29c2b37897370f1a55
Nagesh <nageshdevops8@gmail.com>
2025-08-31 21:38:38 +0530

    shell script updating

---
+++ 03.variables.sh
@@ -0,0 +1,8 @@
+Person1=Trump
+Person2=Musk
+
+echo "$Person1: Hi $Person2, what are you doing"
+echo "$Person2:am doing goood"
+echo "$Person1: what's app $Person2"
+echo "$Person2: goin to Mars, will you come"
+echo "$Person1: sorry, you carry on $Person2. "
