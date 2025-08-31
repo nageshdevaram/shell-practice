7bf7e7614759818111f9a5439ed8e7fec2490e79
Nagesh <nageshdevops8@gmail.com>
2025-08-31 21:44:31 +0530

    shell script updating

---
+++ 04.variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+Person1=$1
+Person2=$2
+
+echo "$Person1: Hi $Person2, what are you doing"
+echo "$Person2:am doing goood"
+echo "$Person1: what's app $Person2"
+echo "$Person2: goin to Mars, will you come"
+echo "$Person1: sorry, you carry on $Person2. "
