eab6a34f29c679f2d131893a11362f6a91a1d7fb
Nagesh <nageshdevops8@gmail.com>
2025-09-13 17:33:10 +0530

    shell script examples

--- 08.practice_examples.sh
+++ 08.practice_examples.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-echo "Shell script started"
 
-person1=$1
-person2=$2
 
-echo "$person1: Hi $person2, what are you doing"
-echo "$person2:am doing goood"
-echo "$person1: what's app $person2"
-echo "$person2: goin to Mars, will you come"
-echo "$person1: sorry, you carry on $person2. "
+echo "Enter your PIN"
+
+read -s PIN
+
+echo "your number is $PIN "
